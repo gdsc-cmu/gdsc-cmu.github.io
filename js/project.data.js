ec5e7932004661e267255eb4bf32e03006c87789
Divya Raj <divya21raj@Divyas-MacBook-Pro.local>
2022-09-22 23:49:04 -0400

    Contact Update

--- js/project.data.js
+++ js/project.data.js
@@ -7,85 +7,12 @@ let projectDetails = [
     id: 1,
     name: "GDSC CMU Website",
     repo: "gdsc-cmu/gdsc-cmu",
-    desc: "Official Website for GDSC CMU, and which is open source 🤩.",
+    desc: "Official Website for GDSC CMU, an open source project 🤩.",
 
     viewLink: "https://gdsc-cmu.github.io/",
     repoLink: "https://github.com/gdsc-cmu/gdsc-cmu.github.io",
 
     viewtarget: "_blank",
-    repotarget: "_blank",
-  },
-  {
-    id: 2,
-    name: "Image Editor",
-    repo: "gdsc-cmu/Image-Editor",
-    desc: "Open Source Web based Image Editor.",
-
-    viewLink: "https://gdsc-cmu.github.io/Image-Editor",
-    repoLink: "https://github.com/gdsc-cmu/Image-Editor",
-
-    viewtarget: "_blank",
-    repotarget: "_blank",
-  },
-  {
-    id: 3,
-    name: "ML Gallery",
-    repo: "gdsc-cmu/ML-Gallery",
-    desc:
-      "A Special Open-Source Repository for all kinds of Data Science and Machine Learning Projects! Contribute your EDA Case Studies, ML Models, Deep Learning Algo, and fun Computer Vision ideas!",
-
-    viewLink: "javascript:void(0)",
-    repoLink: "https://github.com/gdsc-cmu/ML-Gallery",
-
-    viewtarget: "_self",
-    repotarget: "_blank",
-  },
-  {
-    id: 4,
-    name: "Pictionary",
-    repo: "gdsc-cmu/Pictionary",
-    desc: "Ever heard about a Picture based Dictionary? , it's here!",
-
-    viewLink: "https://gdsc-cmu.github.io/Pictionary",
-    repoLink: "https://github.com/gdsc-cmu/Pictionary",
-
-    viewtarget: "_blank",
-    repotarget: "_blank",
-  },
-  {
-    id: 5,
-    name: "UpTrend",
-    repo: "gdsc-cmu/UpTrend",
-    desc: "One place destination to check trending data across websites.",
-
-    viewLink: "javascript:void(0)",
-    repoLink: "https://github.com/gdsc-cmu/UpTrend",
-
-    viewtarget: "_self",
-    repotarget: "_blank",
-  },
-  {
-    id: 6,
-    name: "GuruCool",
-    repo: "gdsc-cmu/GuruCool",
-    desc: "Productivity Solution for Educators and Students.",
-
-    viewLink: "javascript:void(0)",
-    repoLink: "https://github.com/gdsc-cmu/GuruCool",
-
-    viewtarget: "_self",
-    repotarget: "_blank",
-  },
-  {
-    id: 7,
-    name: "Rhythm-Finder",
-    repo: "gdsc-cmu/Rhythm-Finder",
-    desc: "ML-powered Music Recommendation Engine.",
-
-    viewLink: "javascript:void(0)",
-    repoLink: "https://github.com/gdsc-cmu/Rhythm-Finder",
-
-    viewtarget: "_self",
     repotarget: "_blank",
   },
 ];
